a34871ac5868508bfb3e2500a1c258e6e92d584e
Matt Caldwell <matt.caldwell@gmail.com>
2014-05-11 22:51:38 -0400

    add basic validation

--- models.rb
+++ models.rb
@@ -1,6 +1,12 @@
 class Player < ActiveRecord::Base
+  self.primary_key = 'player_id'
 end
 
 class PlayerYear < ActiveRecord::Base
   belongs_to :player
+
+  validates :year, presence: true
+  validates :league, presence: true
+  validates :team, presence: true
+  validates :games, presence: true
 end
